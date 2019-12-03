e684c972032ad4cbcc383e1ca8b2a7c5f9c96610
alanxu <alanxu@cppib.com>
2019-12-03 16:16:25 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,174 @@
+provider "aws" {
+  region = "us-east-1"
+  profile = "cmf"
+}
+
+#
+# EMR IAM resources
+#
+data "aws_iam_policy_document" "emr_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["elasticmapreduce.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "emr_service_role" {
+  name               = "emr${var.environment}ServiceRole"
+  assume_role_policy = "${data.aws_iam_policy_document.emr_assume_role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "emr_service_role" {
+  role       = "${aws_iam_role.emr_service_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceRole"
+}
+
+#
+# EMR IAM resources for EC2
+#
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "emr_ec2_instance_profile" {
+  name               = "${var.environment}JobFlowInstanceProfile"
+  assume_role_policy = "${data.aws_iam_policy_document.ec2_assume_role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "emr_ec2_instance_profile" {
+  role       = "${aws_iam_role.emr_ec2_instance_profile.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforEC2Role"
+}
+
+resource "aws_iam_instance_profile" "emr_ec2_instance_profile" {
+  name = "${aws_iam_role.emr_ec2_instance_profile.name}"
+  role = "${aws_iam_role.emr_ec2_instance_profile.name}"
+}
+
+#
+# Security group resources
+#
+resource "aws_security_group" "emr_master" {
+  vpc_id                 = "${var.vpc_id}"
+  revoke_rules_on_delete = true
+
+  tags = {
+    Name        = "sg${var.name}Master"
+    Project     = "${var.project}"
+    Environment = "${var.environment}"
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "emr_slave" {
+  vpc_id                 = "${var.vpc_id}"
+  revoke_rules_on_delete = true
+
+  tags = {
+    Name        = "sg${var.name}Slave"
+    Project     = "${var.project}"
+    Environment = "${var.environment}"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "emr_svc_access" {
+  vpc_id                 = "${var.vpc_id}"
+  revoke_rules_on_delete = true
+
+  tags = {
+    Name        = "sg${var.name}ServiceAccess"
+    Project     = "${var.project}"
+    Environment = "${var.environment}"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#
+# EMR resources
+#
+resource "aws_emr_cluster" "cluster" {
+  name           = "${var.name}"
+  release_label  = "${var.release_label}"
+  applications   = "${var.applications}"
+  configurations = "${var.configurations}"
+
+  ec2_attributes {
+    key_name                          = "${var.key_name}"
+    subnet_id                         = "${var.subnet_id}"
+    emr_managed_master_security_group = "${aws_security_group.emr_master.id}"
+    emr_managed_slave_security_group  = "${aws_security_group.emr_slave.id}"
+    service_access_security_group  = "${aws_security_group.emr_svc_access.id}"
+    instance_profile                  = "${aws_iam_instance_profile.emr_ec2_instance_profile.arn}"
+  }
+
+  
+  master_instance_group {
+    instance_type = "m4.xlarge"
+  }
+  core_instance_group {
+    instance_type  = "c4.xlarge"
+    instance_count = 2
+
+    ebs_config {
+      size                 = "40"
+      type                 = "gp2"
+      volumes_per_instance = 1
+    }
+  }
+
+  # bootstrap_action {
+  #   path = "${var.bootstrap_uri}"
+  #   name = "${var.bootstrap_name}"
+  #   args = "${var.bootstrap_args}"
+  # }
+
+  # log_uri      = "${var.log_uri}"
+  service_role = "${aws_iam_role.emr_service_role.arn}"
+
+  tags = {
+    Name        = "${var.name}"
+    Project     = "${var.project}"
+    Environment = "${var.environment}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,86 @@
+variable "project" {
+  default = "Unknown"
+}
+
+variable "environment" {
+  default = "Unknown"
+}
+
+variable "name" {
+  default = "my-emr-1"
+}
+
+variable "vpc_id" {
+  default = "vpc-01373d642560468d7"
+}
+
+variable "release_label" {
+  default = "emr-5.28.0"
+}
+
+variable "applications" {
+  default = ["Spark", "Hive", "Hadoop", "Livy", "JupyterHub"]
+  type    = "list"
+}
+
+variable "configurations" {
+  default = <<EOF
+[
+  {
+    "Classification": "spark-env",
+    "Configurations": [
+      {
+        "Classification": "export",
+        "Properties": {
+            "PYSPARK_PYTHON": "/usr/bin/python3"
+          }
+      }
+    ]
+  }
+]
+EOF
+}
+
+variable "key_name" {
+  default = "cmf-ec2"
+}
+
+variable "subnet_id" {
+  default = "subnet-0303e60e78b5262d3"
+}
+
+variable "instance_groups" {
+  default = [
+    {
+      name           = "MasterInstanceGroup"
+      instance_role  = "MASTER"
+      instance_type  = "m3.xlarge"
+      instance_count = 1
+    },
+    {
+      name           = "CoreInstanceGroup"
+      instance_role  = "CORE"
+      instance_type  = "m3.xlarge"
+      instance_count = "4"
+    }
+  ]
+
+  type = "list"
+}
+
+variable "bootstrap_name" {
+  default = ""
+}
+
+variable "bootstrap_uri" {
+  default = ""
+}
+
+variable "bootstrap_args" {
+  default = []
+  type    = "list"
+}
+
+variable "log_uri" {
+  default = ""
+}
